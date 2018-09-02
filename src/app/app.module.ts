35378851f329fd73fbce0bcf2f9890f85d1297d7
Zach Lennane <zach@Zachs-MacBook-Pro.local>
2018-09-02 17:01:21 -0400

    Add resetting vote form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { AppComponent } from './app.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { VoteFormComponent } from './vote-form/vote-form.component';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
 
 
 
--- src/app/vote-form/vote-form.component.ts
+++ src/app/vote-form/vote-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Vote} from '../vote'
+import { Vote } from '../vote'
+import { VoteService } from '../vote.service';
+import { componentRefresh } from '@angular/core/src/render3/instructions';
+
 @Component({
   selector: 'app-vote-form',
   templateUrl: './vote-form.component.html',
@@ -9,20 +12,26 @@ export class VoteFormComponent implements OnInit {
   model = new Vote("", "", "")
   submitted = false;
   constructor() { }
-
+  onSubmit() {
+    this.submitted = true;
+    console.log("onSubmit got called")
+    //this.model.reset();
+  }
   ngOnInit() {
     //this.postData
     //console.log(new Vote(1,"Zach Lennane", "The booty is mine - it belongs to me", "Postiive"));
   }
-  public postData = function(name,subject,type){
+  public postData = function (name, subject, type) {
     console.log("PostData")
     console.log("Name: " + name)
     console.log("Subject: " + subject)
     console.log("Type : " + type)
+    
+    // return componentRefresh;
     //TODO Make a POST Request.
     //TODO Render some kind of a confirmation
 
-    // let myVote = new Vote("Zach Lennane", "The booty is mine - it belongs to me", "Postiive");
+    // let myVote = new Vote("Zach Lennane", "The booty is mine - it belongs to me", "Positive");
     // let doleVote = new Vote("Dylan Tastet", "Wah, I don't like Jonathan Boucher", "Negative");
     // console.log("the submit button was pressed");
     // console.log(myVote);
